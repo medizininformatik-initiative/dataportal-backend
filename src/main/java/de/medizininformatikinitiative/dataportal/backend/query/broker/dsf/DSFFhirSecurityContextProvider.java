b70244cf23360d6db13dd162c957bd6c45025397
Mathias Rühle <math2306@hotmail.com>
2025-12-11 21:19:54 +0100

    Allow DSF Client Key Password to be Null

--- src/main/java/de/medizininformatikinitiative/dataportal/backend/query/broker/dsf/DSFFhirSecurityContextProvider.java
+++ src/main/java/de/medizininformatikinitiative/dataportal/backend/query/broker/dsf/DSFFhirSecurityContextProvider.java
@@ -3,11 +3,14 @@ package de.medizininformatikinitiative.dataportal.backend.query.broker.dsf;
 import de.rwh.utils.crypto.CertificateHelper;
 import de.rwh.utils.crypto.io.CertificateReader;
 import de.rwh.utils.crypto.io.PemIo;
+import org.apache.commons.text.RandomStringGenerator;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.cert.Certificate;
+
+import static com.google.common.base.Strings.isNullOrEmpty;
 
 /**
  * An entity that can provide a security context for communicating with a FHIR server.
@@ -15,15 +18,15 @@ import java.security.cert.Certificate;
 public class DSFFhirSecurityContextProvider implements FhirSecurityContextProvider {
 
   private final String clientKeyFile;
-  private final char[] clientKeyPassword;
+  private final String clientKeyPassword;
   private final String trustCertificateFile;
   private String clientCertificateFile;
 
-  public DSFFhirSecurityContextProvider(String clientCertificateFile, String clientKeyFile, char[] clientKeyPassword,
+  public DSFFhirSecurityContextProvider(String clientCertificateFile, String clientKeyFile, String keyStorePassword,
                                         String trustCertificateFile) {
     this.clientCertificateFile = clientCertificateFile;
     this.clientKeyFile = clientKeyFile;
-    this.clientKeyPassword = clientKeyPassword;
+    this.clientKeyPassword = keyStorePassword;
     this.trustCertificateFile = trustCertificateFile;
   }
 
@@ -38,10 +41,12 @@ public class DSFFhirSecurityContextProvider implements FhirSecurityContextProvid
       if (!Files.isReadable(localClientKeyFile)) {
         throw new IOException("Client key file '" + localClientKeyFile + "' not readable");
       }
+      var password = RandomStringGenerator.builder().withinRange(33, 126).get().generate(16).toCharArray();
       var localKeyStore = CertificateHelper.toJksKeyStore(
-          PemIo.readPrivateKeyFromPem(localClientKeyFile, clientKeyPassword),
+          isNullOrEmpty(clientKeyPassword) ? PemIo.readPrivateKeyFromPem(localClientKeyFile)
+              : PemIo.readPrivateKeyFromPem(localClientKeyFile, clientKeyPassword.toCharArray()),
           new Certificate[]{PemIo.readX509CertificateFromPem(localClientCertificateFile)},
-          "backend-cert", clientKeyPassword);
+          "backend-cert", password);
       var localTrustCertificateFile = Paths.get(trustCertificateFile);
       if (!Files.isReadable(localTrustCertificateFile)) {
         throw new IOException("Certificate file '" + trustCertificateFile + "' not readable");
@@ -49,7 +54,7 @@ public class DSFFhirSecurityContextProvider implements FhirSecurityContextProvid
       var localTrustStore = CertificateReader.allFromCer(localTrustCertificateFile);
 
 
-      return new FhirSecurityContext(localKeyStore, localTrustStore, clientKeyPassword);
+      return new FhirSecurityContext(localKeyStore, localTrustStore, password);
     } catch (Exception e) {
       throw new FhirSecurityContextProvisionException(e);
     }
